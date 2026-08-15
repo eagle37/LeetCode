99a5e0a38a8280da9a933f310216e291a5b7fc0d
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-15 22:36:53 +0530

    File updated at 8/15/2026, 10:36:38 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,16 +1,24 @@
-# Last updated: 8/15/2026, 10:35:40 PM
-1class Solution:
-2    def lengthOfLongestSubstring(self, s: str) -> int:
-3        _ = 0
-4        __ = set()
-5        ___ = 0
-6
-7        for i in range(len(s)):
-8            while s[i] in __:
-9                __.remove(s[_])
-10                _ += 1
-11
-12            __.add(s[i])
-13            ___ = max(___, i - _ + 1)
-14
-15        return ___
+# Last updated: 8/15/2026, 10:36:38 PM
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        freq={}
+        l=0
+        max_len=0
+
+        for i in range(len(s)):
+            
+            
+                
+            while s[i] in freq:
+                    
+                del freq[s[l]]
+                l+=1
+            freq[s[i]]=freq.get(s[i],0)+1
+            
+            max_len=max(max_len,i-l+1)
+            
+                 
+            
+        return max_len            
+
+        
