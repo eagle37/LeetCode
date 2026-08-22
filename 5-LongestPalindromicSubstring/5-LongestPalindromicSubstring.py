bf1054aa00b0e9bd26e85093e05c137eee9f826a
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-22 18:26:06 +0530

    File updated at 8/22/2026, 6:25:50 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,21 +1,12 @@
-# Last updated: 8/22/2026, 6:01:47 PM
+# Last updated: 8/22/2026, 6:25:50 PM
 1class Solution:
-2    def intToRoman(self, num: int) -> str:
-3        d = [
-4            (1000, "M"), (900, "CM"),
-5            (500, "D"), (400, "CD"),
-6            (100, "C"), (90, "XC"),
-7            (50, "L"), (40, "XL"),
-8            (10, "X"), (9, "IX"),
-9            (5, "V"), (4, "IV"),
-10            (1, "I")
-11        ]
-12
-13        res = ""
-14
-15        for value, symbol in d:
-16            while num >= value:
-17                res += symbol
-18                num -= value
-19
-20        return res
+2    def longestCommonPrefix(self, strs: List[str]) -> str:
+3        prefix = ""
+4        for i in range(len(strs[0])):
+5            for s in strs:
+6                if i >= len(s) or s[i] != strs[0][i]:
+7                    return prefix
+8
+9            prefix += strs[0][i]
+10
+11        return prefix
