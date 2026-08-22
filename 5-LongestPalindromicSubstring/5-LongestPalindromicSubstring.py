82a7df3bc45746042d991cbe636d005abc80ebe7
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-22 19:00:19 +0530

    File updated at 8/22/2026, 7:00:04 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,13 +1,33 @@
-# Last updated: 8/22/2026, 6:26:03 PM
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        if len(strs)==0:
-            return ""
-        d = ""
-        for i in range(len(strs[0])):
-            cur = strs[0][i]
-            for j in range(1,len(strs)):
-                if (i>=len(strs[j])) or (strs[j][i]!=cur):
-                    return d
-            d+=cur
-        return d
+# Last updated: 8/22/2026, 7:00:04 PM
+1class Solution:
+2    def threeSum(self, nums: list[int]) -> list[list[int]]:
+3        nums.sort()
+4        li = []
+5
+6        for i in range(len(nums) - 2):
+7            if i > 0 and nums[i] == nums[i - 1]:
+8                continue
+9
+10            l = i + 1
+11            r = len(nums) - 1
+12
+13            while l < r:
+14                total = nums[i] + nums[l] + nums[r]
+15
+16                if total == 0:
+17                    li.append([nums[i], nums[l], nums[r]])
+18                    l += 1
+19                    r -= 1
+20
+21                    while l < r and nums[l] == nums[l - 1]:
+22                        l += 1
+23
+24                    while l < r and nums[r] == nums[r + 1]:
+25                        r -= 1
+26
+27                elif total < 0:
+28                    l += 1
+29                else:
+30                    r -= 1
+31
+32        return li
