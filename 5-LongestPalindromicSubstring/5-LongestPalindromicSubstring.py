173abd0a6d54ff37f0165c02e9bc6beac30e20f2
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-22 18:26:18 +0530

    File updated at 8/22/2026, 6:26:03 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,12 +1,13 @@
-# Last updated: 8/22/2026, 6:25:50 PM
-1class Solution:
-2    def longestCommonPrefix(self, strs: List[str]) -> str:
-3        prefix = ""
-4        for i in range(len(strs[0])):
-5            for s in strs:
-6                if i >= len(s) or s[i] != strs[0][i]:
-7                    return prefix
-8
-9            prefix += strs[0][i]
-10
-11        return prefix
+# Last updated: 8/22/2026, 6:26:03 PM
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs)==0:
+            return ""
+        d = ""
+        for i in range(len(strs[0])):
+            cur = strs[0][i]
+            for j in range(1,len(strs)):
+                if (i>=len(strs[j])) or (strs[j][i]!=cur):
+                    return d
+            d+=cur
+        return d
