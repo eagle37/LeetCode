bd3b4d55c6096fba609dd57944c0132437cd2ca9
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-22 17:43:05 +0530

    File updated at 8/22/2026, 5:42:49 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,23 +1,15 @@
-# Last updated: 8/22/2026, 5:26:30 PM
+# Last updated: 8/22/2026, 5:42:49 PM
 1class Solution:
-2    def longestPalindrome(self, s: str) -> str:
-3        n = len(s)
-4        res = ""
-5
-6        for i in range(n):
-7            st = end = i
-8            while st >= 0 and end < n and s[st] == s[end]:
-9                st -= 1
-10                end += 1
-11            temp = s[st+1:end]
-12            if len(temp) > len(res):
-13                res = temp
-14            st, end = i, i+1
-15            while st >= 0 and end < n and s[st] == s[end]:
-16                st -= 1
-17                end += 1
-18            temp = s[st+1:end]
-19            if len(temp) > len(res):
-20                res = temp
-21
-22        return res
+2    def maxArea(self, height: List[int]) -> int:
+3        l = 0
+4        r = len(height)-1
+5        a = 0
+6        while l < r:
+7            area = (r-l) * min(height[l], height[r])
+8            if area > a:
+9                a = area
+10            if height[l] < height[r]:
+11                l += 1
+12            else:
+13                r -= 1
+14        return a
