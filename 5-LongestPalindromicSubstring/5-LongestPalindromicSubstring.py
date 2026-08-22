859de39371b525babe13b671a23412e5495e3757
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-22 17:26:45 +0530

    File updated at 8/22/2026, 5:26:30 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,25 +1,23 @@
-# Last updated: 8/16/2026, 11:42:58 PM
+# Last updated: 8/22/2026, 5:26:30 PM
 1class Solution:
-2    def myAtoi(self, s: str) -> int:
-3        res = 0
-4        sign = 1
-5        s = s.lstrip()
-6
-7        if not s:
-8            return 0
-9
-10        if s[0] == "+":
-11            sign = 1
-12        elif s[0] == "-":
-13            sign = -1
-14
-15        for i in range(len(s)):
-16            if i == 0 and s[i] in ("+", "-"):
-17                continue
-18            elif not "0" <= s[i] <= "9":
-19                break
-20            else:
-21                res = res * 10 + int(s[i])
-22
-23        res *= sign
-24        return max(-2**31, min(res, 2**31 - 1))
+2    def longestPalindrome(self, s: str) -> str:
+3        n = len(s)
+4        res = ""
+5
+6        for i in range(n):
+7            st = end = i
+8            while st >= 0 and end < n and s[st] == s[end]:
+9                st -= 1
+10                end += 1
+11            temp = s[st+1:end]
+12            if len(temp) > len(res):
+13                res = temp
+14            st, end = i, i+1
+15            while st >= 0 and end < n and s[st] == s[end]:
+16                st -= 1
+17                end += 1
+18            temp = s[st+1:end]
+19            if len(temp) > len(res):
+20                res = temp
+21
+22        return res
