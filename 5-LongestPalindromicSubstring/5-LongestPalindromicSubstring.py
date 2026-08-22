42c12525a2abe23cba803f3946e6f5b95d0624b4
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-22 18:01:48 +0530

    File updated at 8/22/2026, 6:01:33 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,15 +1,21 @@
-# Last updated: 8/22/2026, 5:52:57 PM
+# Last updated: 8/22/2026, 6:01:33 PM
 1class Solution:
-2    def maxArea(self, height: List[int]) -> int:
-3        l = 0
-4        r = len(height)-1
-5        a = 0
-6        while l < r:
-7            area = (r-l) * min(height[l], height[r])
-8            if area > a:
-9                a = area
-10            if height[l] < height[r]:
-11                l += 1
-12            else:
-13                r -= 1
-14        return a
+2    def intToRoman(self, num: int) -> str:
+3        d = [
+4            (1000, "M"), (900, "CM"),
+5            (500, "D"), (400, "CD"),
+6            (100, "C"), (90, "XC"),
+7            (50, "L"), (40, "XL"),
+8            (10, "X"), (9, "IX"),
+9            (5, "V"), (4, "IV"),
+10            (1, "I")
+11        ]
+12
+13        res = ""
+14
+15        for value, symbol in d:
+16            while num >= value:
+17                res += symbol
+18                num -= value
+19
+20        return res
