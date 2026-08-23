759dcf93bc38fa48925438efffcd04155b7cbaae
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-23 18:23:22 +0530

    File updated at 8/23/2026, 6:23:06 PM

--- 16-3SumClosest/16-3SumClosest.py
+++ 16-3SumClosest/16-3SumClosest.py
@@ -1,24 +1,22 @@
-# Last updated: 8/23/2026, 6:11:28 PM
+# Last updated: 8/23/2026, 6:23:06 PM
 1class Solution:
-2    def letterCombinations(self, digits: str) -> List[str]:
-3        d = {
-4            "2": "abc", 
-5            "3": "def", 
-6            "4": "ghi", 
-7            "5": "jkl", 
-8            "6": "mno",
-9            "7": "pqrs",
-10            "8": "tuv",
-11            "9": "wxyz"
-12        }
-13
-14        rs = []
-15
-16        def backtrack(pos, curr):
-17            if pos == len(digits):
-18                rs.append(curr)
-19                return
-20            for ch in d[digits[pos]]:
-21                backtrack(pos+1, curr+ch)
-22        backtrack(0, "")
-23        return rs
+2    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+3        ans = []
+4        nums.sort()
+5        for i in range(len(nums)):
+6            for j in range(i+1, len(nums)):
+7                l, r = j+1, len(nums)-1
+8                while l < r:
+9                    if nums[l] + nums[r] < target - nums[i] - nums[j]:
+10                        l += 1
+11                    elif nums[l] + nums[r] > target - nums[i] - nums[j]:
+12                        r -= 1
+13                    elif nums[l] + nums[r] == target - nums[i] - nums[j]:
+14                        if [nums[i], nums[j], nums[l], nums[r]] not in ans:
+15                            ans.append([nums[i], nums[j], nums[l], nums[r]])
+16                            l += 1
+17                            r -= 1
+18                        else:
+19                            l += 1
+20                            r -= 1
+21        return ans
