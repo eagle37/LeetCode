6c924af3e9c4e31a5c0f6070b0eeaeaefc604e7e
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-23 18:27:28 +0530

    File updated at 8/23/2026, 6:27:13 PM

--- 16-3SumClosest/16-3SumClosest.py
+++ 16-3SumClosest/16-3SumClosest.py
@@ -1,22 +1,22 @@
-# Last updated: 8/23/2026, 6:23:06 PM
+# Last updated: 8/23/2026, 6:27:13 PM
 1class Solution:
 2    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
 3        ans = []
-4        nums.sort()
-5        for i in range(len(nums)):
-6            for j in range(i+1, len(nums)):
-7                l, r = j+1, len(nums)-1
-8                while l < r:
-9                    if nums[l] + nums[r] < target - nums[i] - nums[j]:
-10                        l += 1
-11                    elif nums[l] + nums[r] > target - nums[i] - nums[j]:
-12                        r -= 1
-13                    elif nums[l] + nums[r] == target - nums[i] - nums[j]:
-14                        if [nums[i], nums[j], nums[l], nums[r]] not in ans:
-15                            ans.append([nums[i], nums[j], nums[l], nums[r]])
-16                            l += 1
-17                            r -= 1
-18                        else:
-19                            l += 1
-20                            r -= 1
+4        seen = set()
+5        nums.sort()
+6        for i in range(len(nums)):
+7            for j in range(i+1, len(nums)):
+8                l, r = j+1, len(nums)-1
+9                while l < r:
+10                    if nums[l] + nums[r] < target - nums[i] - nums[j]:
+11                        l += 1
+12                    elif nums[l] + nums[r] > target - nums[i] - nums[j]:
+13                        r -= 1
+14                    elif nums[l] + nums[r] == target - nums[i] - nums[j]:
+15                        x = (nums[i], nums[j], nums[l], nums[r])
+16                        if x not in seen:
+17                            seen.add(x)
+18                            ans.append([nums[i], nums[j], nums[l], nums[r]])
+19                        l += 1
+20                        r -= 1
 21        return ans
