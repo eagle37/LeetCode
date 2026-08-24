a02acccd9b8f619e3665ed57b08e226bf0f04395
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-24 21:48:42 +0530

    Create 2002-StoneGameViii

---
+++ 2002-StoneGameViii/2002-StoneGameViii.py
@@ -0,0 +1,15 @@
+# Last updated: 8/24/2026, 9:48:26 PM
+class Solution:
+    def stoneGameVIII(self, stones):
+        n = len(stones)
+        prefix = [0] * n
+        prefix[0] = stones[0]
+
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] + stones[i]
+        ans = prefix[n - 1]
+
+        for i in range(n - 2, 0, -1):
+            ans = max(ans, prefix[i] - ans)
+
+        return ans
