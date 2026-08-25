d91dc2d93064654916e2030d470d92e92d5761d0
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-25 23:58:37 +0530

    File updated at 8/25/2026, 11:58:20 PM

--- 23-MergekSortedLists/23-MergekSortedLists.py
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -1,26 +1,45 @@
-# Last updated: 8/25/2026, 11:10:40 PM
-1# Definition for singly-linked list.
-2# class ListNode:
-3#     def __init__(self, val=0, next=None):
-4#         self.val = val
-5#         self.next = next
-6class Solution:
-7    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+# Last updated: 8/25/2026, 11:58:20 PM
+1class Solution:
+2    def searchRange(self, nums: List[int], target: int) -> List[int]:
+3
+4        def first_position():
+5            left = 0
+6            right = len(nums) - 1
+7            ans = -1
 8
-9        dummy = ListNode(0)
-10        dummy.next = head
+9            while left <= right:
+10                mid = left + (right - left) // 2
 11
-12        prev = dummy
-13
-14        while prev.next and prev.next.next:
+12                if nums[mid] == target:
+13                    ans = mid
+14                    right = mid - 1
 15
-16            first = prev.next
-17            second = first.next
+16                elif nums[mid] < target:
+17                    left = mid + 1
 18
-19            first.next = second.next
-20            second.next = first
-21            prev.next = second
-22
-23            prev = first
-24
-25        return dummy.next 
+19                else:
+20                    right = mid - 1
+21
+22            return ans
+23
+24        def last_position():
+25            left = 0
+26            right = len(nums) - 1
+27            ans = -1
+28
+29            while left <= right:
+30                mid = left + (right - left) // 2
+31
+32                if nums[mid] == target:
+33                    ans = mid
+34                    left = mid + 1
+35
+36                elif nums[mid] < target:
+37                    left = mid + 1
+38
+39                else:
+40                    right = mid - 1
+41
+42            return ans
+43
+44        return [first_position(), last_position()]
