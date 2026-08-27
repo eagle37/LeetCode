97b6af7ee880469479027bcac6403ee5b23dbf95
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-27 15:37:30 +0530

    File updated at 8/27/2026, 3:37:14 PM

--- 36-ValidSudoku/36-ValidSudoku.py
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -1,28 +1,52 @@
-# Last updated: 8/27/2026, 3:16:13 PM
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        #rows = [[] for _ in range(9)]
-        # cols = [[] for _ in range(9)]
-        # box = [[] for _ in range(9)]
-        # go through each cell in board, add the num to each the row, col, box it is in, if same num appear in the these, return Flase
-        
-        rows = [[] for _ in range(len(board))]
-        cols = [[] for _ in range(len(board[0]))]
-        boxes = [[] for _ in range(9)]
-
-        for r in range(9):
-            for c in range(9):
-                num = board[r][c]
-                box = (r // 3) * 3 + c // 3
-                
-                if num == ".":
-                    continue
-                elif num in rows[r] or num in cols[c] or num in boxes[box]:
-                    return False
-                
-                else:
-                    rows[r].append(num)
-                    cols[c].append(num)
-                    boxes[box].append(num)
-        
-        return True
+# Last updated: 8/27/2026, 3:37:14 PM
+1class Solution:
+2    def solveSudoku(self, board: List[List[str]]) -> None:
+3
+4        rows = [set() for _ in range(9)]
+5        cols = [set() for _ in range(9)]
+6        boxes = [set() for _ in range(9)]
+7
+8        for r in range(9):
+9            for c in range(9):
+10
+11                if board[r][c] != '.':
+12                    num = board[r][c]
+13                    box = (r // 3) * 3 + (c // 3)
+14
+15                    rows[r].add(num)
+16                    cols[c].add(num)
+17                    boxes[box].add(num)
+18
+19        def backtrack():
+20
+21            for r in range(9):
+22                for c in range(9):
+23
+24                    if board[r][c] == '.':
+25
+26                        box = (r // 3) * 3 + (c // 3)
+27
+28                        for num in "123456789":
+29
+30                            if (num not in rows[r] and
+31                                num not in cols[c] and
+32                                num not in boxes[box]):
+33
+34                                board[r][c] = num
+35                                rows[r].add(num)
+36                                cols[c].add(num)
+37                                boxes[box].add(num)
+38
+39                                if backtrack():
+40                                    return True
+41
+42                                board[r][c] = '.'
+43                                rows[r].remove(num)
+44                                cols[c].remove(num)
+45                                boxes[box].remove(num)
+46
+47                        return False
+48
+49            return True
+50
+51        backtrack()
