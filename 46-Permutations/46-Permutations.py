de4395cb9e314ee47c8f0760272a908095faaa62
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-29 20:31:48 +0530

    File updated at 8/29/2026, 8:31:31 PM

--- 46-Permutations/46-Permutations.py
+++ 46-Permutations/46-Permutations.py
@@ -1,28 +1,31 @@
-# Last updated: 8/29/2026, 8:31:18 PM
-1class Solution:
-2    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-3        nums.sort()
-4        res = []
-5        path = []
-6        used = [False] * len(nums)
-7
-8        def backtrack():
-9            if len(path) == len(nums):
-10                res.append(path.copy())
-11                return
-12            
-13            for i in range(len(nums)):
-14                if used[i]:
-15                    continue
-16                if i > 0 and nums[i] == nums[i-1] and used[i-1]:
-17                    continue
-18                used[i] = True
-19                path.append(nums[i])
-20
-21                backtrack()
-22
-23                path.pop()
-24                used[i] = False
-25        
-26        backtrack()
-27        return res
+# Last updated: 8/29/2026, 8:31:31 PM
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        visit = [False] * len(nums)
+        if len(nums) == 1:
+            return [nums[:]]
+
+        def backtrack(ans, path):
+
+            if len(path) == len(nums):
+                return ans.append(path[:])
+
+            for i in range(len(nums)):
+                if visit[i]:
+                    continue
+
+                if i > 0 and nums[i] == nums[i - 1] and not visit[i - 1]:
+                    continue
+
+                visit[i] = True
+                path.append(nums[i])
+
+                backtrack(ans, path)
+
+                path.pop()
+                visit[i] = False
+        nums.sort()
+        backtrack(ans, [])
+        return ans
