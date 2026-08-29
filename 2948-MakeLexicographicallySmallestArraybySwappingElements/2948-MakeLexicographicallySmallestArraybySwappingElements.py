6149424bd14c4d303e9721df268477136b48ee5f
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-29 20:14:48 +0530

    File updated at 8/29/2026, 8:14:31 PM

--- 2948-MakeLexicographicallySmallestArraybySwappingElements/2948-MakeLexicographicallySmallestArraybySwappingElements.py
+++ 2948-MakeLexicographicallySmallestArraybySwappingElements/2948-MakeLexicographicallySmallestArraybySwappingElements.py
@@ -1,22 +1,19 @@
-# Last updated: 8/29/2026, 8:14:16 PM
-1class Solution:
-2    def lexicographicallySmallestArray(self, nums, limit):
-3        n = len(nums)
-4        arr = sorted((num, i) for i, num in enumerate(nums))
-5
-6        i = 0
-7
-8        while i < n:
-9            j = i
-10
-11            while j + 1 < n and arr[j + 1][0] - arr[j][0] <= limit:
-12                j += 1
-13
-14            indices = sorted(arr[k][1] for k in range(i, j + 1))
-15
-16            for k in range(len(indices)):
-17                nums[indices[k]] = arr[i + k][0]
-18
-19            i = j + 1
-20
-21        return nums
+# Last updated: 8/29/2026, 8:14:31 PM
+class Solution:
+    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+        groups=[]
+        cur=[]
+        d = {}
+        for x in sorted(nums):
+            if not cur or x-cur[-1] <= limit:
+                cur.append(x)
+            else:
+                groups.append(cur.copy())
+                cur = [x]
+            d[x] = len(groups)
+
+        groups.append(cur.copy())
+        groups = [l[::-1] for l in groups]
+
+        return [groups[d[x]].pop() for x in nums]
+        
