474df2c0a4da4968c35823ecbbed293974ad7968
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-29 20:31:35 +0530

    File updated at 8/29/2026, 8:31:18 PM

--- 46-Permutations/46-Permutations.py
+++ 46-Permutations/46-Permutations.py
@@ -1,26 +1,28 @@
-# Last updated: 8/29/2026, 8:28:59 PM
+# Last updated: 8/29/2026, 8:31:18 PM
 1class Solution:
 2    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-3        res = []
-4        path = []
-5        used = [False] * len(nums)
-6
-7        def backtrack():
-8            if len(path) == len(nums) and path not in res:
-9                res.append(path.copy())
-10                return
-11            
-12            for i in range(len(nums)):
-13                if used[i]:
-14                    continue
-15                
-16                used[i] = True
-17                path.append(nums[i])
-18
-19                backtrack()
+3        nums.sort()
+4        res = []
+5        path = []
+6        used = [False] * len(nums)
+7
+8        def backtrack():
+9            if len(path) == len(nums):
+10                res.append(path.copy())
+11                return
+12            
+13            for i in range(len(nums)):
+14                if used[i]:
+15                    continue
+16                if i > 0 and nums[i] == nums[i-1] and used[i-1]:
+17                    continue
+18                used[i] = True
+19                path.append(nums[i])
 20
-21                path.pop()
-22                used[i] = False
-23        
-24        backtrack()
-25        return res
+21                backtrack()
+22
+23                path.pop()
+24                used[i] = False
+25        
+26        backtrack()
+27        return res
