a831a2314049cf2565c73e624922a850e20340db
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-29 20:39:41 +0530

    File updated at 8/29/2026, 8:39:24 PM

--- 46-Permutations/46-Permutations.py
+++ 46-Permutations/46-Permutations.py
@@ -1,26 +1,10 @@
-# Last updated: 8/29/2026, 8:33:08 PM
-1class Solution:
-2    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-3        res = []
-4
-5        def backtrack(start):
-6            if start == len(nums):
-7                res.append(nums.copy())
-8                return
-9
-10            seen = set()
-11
-12            for i in range(start, len(nums)):
-13                if nums[i] in seen:
-14                    continue
-15
-16                seen.add(nums[i])
-17
-18                nums[start], nums[i] = nums[i], nums[start]
-19
-20                backtrack(start + 1)
-21
-22                nums[start], nums[i] = nums[i], nums[start]
-23
-24        backtrack(0)
-25        return res
+# Last updated: 8/29/2026, 8:39:24 PM
+1import numpy as np
+2class Solution:
+3    def rotate(self, matrix: List[List[int]]) -> None:
+4        """
+5        Do not return anything, modify matrix in-place instead.
+6        """
+7        arr = np.array(matrix)
+8        arr = np.rot90(arr, k=-1)
+9        matrix[:] = arr.tolist()
