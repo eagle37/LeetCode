8fd1d1031aa0ad150fc10f10926070eef8c10802
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-29 20:47:42 +0530

    File updated at 8/29/2026, 8:47:25 PM

--- 46-Permutations/46-Permutations.py
+++ 46-Permutations/46-Permutations.py
@@ -1,16 +1,4 @@
-# Last updated: 8/29/2026, 8:46:27 PM
+# Last updated: 8/29/2026, 8:47:25 PM
 1class Solution:
-2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-3
-4        groups = {}
-5
-6        for word in strs:
-7            key = ''.join(sorted(word))
-8
-9            if key not in groups:
-10                groups[key] = []
-11
-12            groups[key].append(word)
-13
-14        return list(groups.values())
-15
+2    def myPow(self, x: float, n: int) -> float:
+3        return x ** n
