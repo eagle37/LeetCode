a081ca956d75dd60b524ef5e03740daaca2b22c4
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-29 20:27:24 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.py
@@ -0,0 +1,26 @@
+# Last updated: 8/29/2026, 8:27:07 PM
+1class Solution:
+2    def permute(self, nums: List[int]) -> List[List[int]]:
+3        res = []
+4        path = []
+5        used = [False] * len(nums)
+6
+7        def backtrack():
+8            if len(path) == len(nums):
+9                res.append(path.copy())
+10                return
+11            
+12            for i in range(len(nums)):
+13                if used[i]:
+14                    continue
+15                
+16                used[i] = True
+17                path.append(nums[i])
+18
+19                backtrack()
+20
+21                path.pop()
+22                used[i] = False
+23        
+24        backtrack()
+25        return res
