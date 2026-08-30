aa75d038eee55ee1312f04cc571c3687eaecdafa
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-08-30 16:07:54 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.py
@@ -0,0 +1,11 @@
+# Last updated: 8/30/2026, 4:07:37 PM
+1class Solution:
+2    def canJump(self, nums: List[int]) -> bool:
+3        i = 0
+4        res = False
+5        for j in range(len(nums)):
+6            if i < j:
+7                return res
+8            i = max(i, j+nums[j])
+9            if i >= len(nums)-1:
+10                return True
