09034213e4a74768b44ee3f62aba631e529e3dda
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-01 23:27:03 +0530

    File updated at 9/1/2026, 11:26:46 PM

--- 61-RotateList/61-RotateList.py
+++ 61-RotateList/61-RotateList.py
@@ -1,20 +1,34 @@
-# Last updated: 9/1/2026, 11:21:58 PM
-1class Solution:
-2    def plusOne(self, digits: List[int]) -> List[int]:
-3        if digits[-1] < 9:
-4            digits[-1] += 1
-5            return digits
-6        s = 0
-7        k = 1
-8        for i in digits[::-1]:
-9            s += i * k
-10            k *= 10
-11        s += 1
-12        j = -1
-13        li = []
-14        while s > 0:
-15            d = s % 10
-16            s = s // 10
-17            li.insert(j, d)
-18            j -= 1
-19        return li
+# Last updated: 9/1/2026, 11:26:46 PM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+8        if not head or not head.next or k == 0:
+9            return head
+10
+11        length = 1
+12        tail = head
+13
+14        while tail.next:
+15            tail = tail.next
+16            length += 1
+17
+18        k %= length
+19
+20        if k == 0:
+21            return head
+22
+23        new_tail = head
+24
+25        for _ in range(length - k - 1):
+26            new_tail = new_tail.next
+27
+28        new_head = new_tail.next
+29
+30        new_tail.next = None
+31        tail.next = head
+32
+33        return new_head
