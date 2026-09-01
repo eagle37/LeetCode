2c5c5840c81ae9473f9ef5b8a4f017a41413c318
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-01 23:29:20 +0530

    Update 66-PlusOne.py

--- 66-PlusOne/66-PlusOne.py
+++ 66-PlusOne/66-PlusOne.py
@@ -1,6 +1,20 @@
-# Last updated: 8/15/2026, 4:32:28 PM
+# Last updated: 9/01/2026, 11:28:28 PM
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        n = int("".join(map(str,digits)))
-        n += 1
-        return [int(d) for d in str(n)]
+        if digits[-1] < 9:
+            digits[-1] += 1
+            return digits
+        s = 0
+        k = 1
+        for i in digits[::-1]:
+            s += i * k
+            k *= 10
+        s += 1
+        j = -1
+        li = []
+        while s > 0:
+            d = s % 10
+            s = s // 10
+            li.insert(j, d)
+            j -= 1
+        return li
