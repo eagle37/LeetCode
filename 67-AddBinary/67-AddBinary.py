65fc93c510c3afea02acf99f5ffb86bc98a4d0c5
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-01 23:37:49 +0530

    Update 67-AddBinary.py

--- 67-AddBinary/67-AddBinary.py
+++ 67-AddBinary/67-AddBinary.py
@@ -1,4 +1,14 @@
-# Last updated: 8/15/2026, 4:32:25 PM
+# Last updated: 9/1/2026, 11:37:25 PM
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a,2)+int(b,2))[2:]
+        if a == '0' and b == '0':
+            return "0"
+        a = int(a, 2)
+        b = int(b, 2)
+        c = a+b
+        s = ""
+        while c > 0:
+            k = c % 2
+            c = c // 2
+            s = str(k) + s
+        return s
