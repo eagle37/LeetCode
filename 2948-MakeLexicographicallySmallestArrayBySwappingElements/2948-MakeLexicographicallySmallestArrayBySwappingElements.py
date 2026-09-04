d323160df8acf7ae26b41ab69b20bf5fb7f0766d
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-04 15:33:00 +0530

    Create 2948-MakeLexicographicallySmallestArrayBySwappingElements

---
+++ 2948-MakeLexicographicallySmallestArrayBySwappingElements/2948-MakeLexicographicallySmallestArrayBySwappingElements.py
@@ -0,0 +1,22 @@
+# Last updated: 9/4/2026, 3:32:42 PM
+class Solution:
+    def lexicographicallySmallestArray(self, nums, limit):
+        n = len(nums)
+        arr = sorted((num, i) for i, num in enumerate(nums))
+
+        i = 0
+
+        while i < n:
+            j = i
+
+            while j + 1 < n and arr[j + 1][0] - arr[j][0] <= limit:
+                j += 1
+
+            indices = sorted(arr[k][1] for k in range(i, j + 1))
+
+            for k in range(len(indices)):
+                nums[indices[k]] = arr[i + k][0]
+
+            i = j + 1
+
+        return nums
