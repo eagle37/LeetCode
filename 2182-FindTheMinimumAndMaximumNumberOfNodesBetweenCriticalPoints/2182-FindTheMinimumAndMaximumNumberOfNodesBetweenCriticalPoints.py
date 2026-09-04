66562dc849772e0b6590b277c6285098097491e7
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-04 15:23:41 +0530

    Create 2182-FindTheMinimumAndMaximumNumberOfNodesBetweenCriticalPoints

---
+++ 2182-FindTheMinimumAndMaximumNumberOfNodesBetweenCriticalPoints/2182-FindTheMinimumAndMaximumNumberOfNodesBetweenCriticalPoints.py
@@ -0,0 +1,37 @@
+# Last updated: 9/4/2026, 3:23:23 PM
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+
+        critical = []
+
+        prev = head
+        curr = head.next
+        nxt = curr.next
+
+        i = 1
+
+        while nxt:
+            if ((curr.val > prev.val and curr.val > nxt.val) or
+                (curr.val < prev.val and curr.val < nxt.val)):
+
+                critical.append(i)
+
+            prev = curr
+            curr = nxt
+            nxt = nxt.next
+            i += 1
+
+        if len(critical) < 2:
+            return [-1, -1]
+
+        max_dist = critical[-1] - critical[0]
+
+        min_dist = float('inf')
+
+        for i in range(1, len(critical)):
+            min_dist = min(
+                min_dist,
+                critical[i] - critical[i - 1]
+            )
+
+        return [min_dist, max_dist]
