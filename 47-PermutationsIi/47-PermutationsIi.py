8d989e46c572c045317f5a78c773f92bc11bc7a6
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-04 15:24:52 +0530

    Create 47-PermutationsIi

---
+++ 47-PermutationsIi/47-PermutationsIi.py
@@ -0,0 +1,26 @@
+# Last updated: 9/4/2026, 3:24:34 PM
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(start):
+            if start == len(nums):
+                res.append(nums.copy())
+                return
+
+            seen = set()
+
+            for i in range(start, len(nums)):
+                if nums[i] in seen:
+                    continue
+
+                seen.add(nums[i])
+
+                nums[start], nums[i] = nums[i], nums[start]
+
+                backtrack(start + 1)
+
+                nums[start], nums[i] = nums[i], nums[start]
+
+        backtrack(0)
+        return res
