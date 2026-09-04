7af38b7eefe604c51b5f201090e2add6ec5cf121
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-04 16:04:49 +0530

    File updated at 9/4/2026, 4:04:31 PM

--- 71-SimplifyPath/71-SimplifyPath.py
+++ 71-SimplifyPath/71-SimplifyPath.py
@@ -1,11 +1,10 @@
-# Last updated: 9/4/2026, 4:04:15 PM
-1class Solution:
-2    def removeDuplicates(self, nums: List[int]) -> int:
-3        k = 2
-4
-5        for i in range(2, len(nums)):
-6            if nums[i] != nums[k - 2]:
-7                nums[k] = nums[i]
-8                k += 1
-9
-10        return k
+# Last updated: 9/4/2026, 4:04:31 PM
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n=len(nums)
+        k = 2
+        for i in range(2,n):
+            if nums[i] != nums[k - 2]:
+                nums[k] = nums[i]
+                k += 1
+        return k
