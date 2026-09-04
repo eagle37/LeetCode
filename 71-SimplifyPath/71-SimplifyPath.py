ee1f5f48c4909ba2c2bf94c8d02d421210f582f3
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-04 16:12:00 +0530

    File updated at 9/4/2026, 4:11:42 PM

--- 71-SimplifyPath/71-SimplifyPath.py
+++ 71-SimplifyPath/71-SimplifyPath.py
@@ -1,4 +1,33 @@
-# Last updated: 9/4/2026, 4:06:56 PM
+# Last updated: 9/4/2026, 4:11:42 PM
 1class Solution:
 2    def search(self, nums: List[int], target: int) -> bool:
-3        return target in nums
+3
+4        left = 0
+5        right = len(nums) - 1
+6
+7        while left <= right:
+8
+9            mid = (left + right) // 2
+10
+11            if nums[mid] == target:
+12                return True
+13
+14            if nums[left] == nums[mid] == nums[right]:
+15                left += 1
+16                right -= 1
+17
+18            elif nums[left] <= nums[mid]:
+19
+20                if nums[left] <= target < nums[mid]:
+21                    right = mid - 1
+22                else:
+23                    left = mid + 1
+24
+25            else:
+26
+27                if nums[mid] < target <= nums[right]:
+28                    left = mid + 1
+29                else:
+30                    right = mid - 1
+31
+32        return False
