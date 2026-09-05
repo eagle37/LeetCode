a3361d9cbf1a56876e47ede20f510122d619ddac
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-05 17:27:30 +0530

    File updated at 9/5/2026, 5:27:13 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,8 +1,10 @@
-# Last updated: 9/5/2026, 5:26:30 PM
-1class Solution:
-2    def isPalindrome(self, s: str) -> bool:
-3        x = ''
-4        for i in range(len(s)-1, -1, -1):
-5            if s[i].isalnum():
-6                x += s[i].lower()
-7        return x == x[::-1]
+# Last updated: 9/5/2026, 5:27:13 PM
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        c = ""
+        for ch in s:
+            if ch.isalnum():
+                c += ch
+        return c == c[::-1]         
+        
