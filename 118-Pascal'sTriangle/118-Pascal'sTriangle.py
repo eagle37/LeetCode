b78d6638926accedb0c6704c30120bf279f83f4f
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-05 17:00:52 +0530

    Create 118-Pascal'sTriangle

---
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.py
@@ -0,0 +1,19 @@
+# Last updated: 9/5/2026, 5:00:35 PM
+1class Solution:
+2    def generate(self, numRows: int) -> List[List[int]]:
+3
+4        ans = [[1]]
+5
+6        for i in range(1, numRows):
+7
+8            prev = ans[-1]
+9            row = [1]
+10
+11            for j in range(1, i):
+12                row.append(prev[j-1] + prev[j])
+13
+14            row.append(1)
+15
+16            ans.append(row)
+17
+18        return ans
