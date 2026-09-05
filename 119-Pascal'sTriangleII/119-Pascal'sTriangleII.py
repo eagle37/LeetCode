8cb2db9a943f37d33f7e71b1684c07918e9b1ec6
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-05 16:58:33 +0530

    File updated at 9/5/2026, 4:58:16 PM

--- 119-Pascal'sTriangleII/119-Pascal'sTriangleII.py
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.py
@@ -1,18 +1,13 @@
-# Last updated: 9/5/2026, 4:57:33 PM
-1class Solution:
-2    def getRow(self, rowIndex: int) -> List[int]:
-3        ans = [1]
-4
-5        for i in range(1, rowIndex+1):
-6
-7            prev = ans
-8            row = [1]
-9
-10            for j in range(1, i):
-11                row.append(prev[j-1] + prev[j])
-12
-13            row.append(1)
-14
-15            ans = row
-16
-17        return ans
+# Last updated: 9/5/2026, 4:58:16 PM
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        triangle = []
+        for i in range(rowIndex+1):
+            a = [1]*(i+1)
+            for j in range(1,i):
+                a[j] = triangle[i-1][j-1] + triangle[i-1][j]
+
+
+            triangle.append(a)
+
+        return triangle[rowIndex]
