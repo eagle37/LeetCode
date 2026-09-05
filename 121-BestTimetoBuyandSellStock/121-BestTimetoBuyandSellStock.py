6ec2add2672f6ed5919b4241a9df4c88cce24516
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-05 17:26:01 +0530

    File updated at 9/5/2026, 5:25:44 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,4 +1,8 @@
-# Last updated: 9/5/2026, 5:24:49 PM
+# Last updated: 9/5/2026, 5:25:44 PM
 1class Solution:
 2    def isPalindrome(self, s: str) -> bool:
-3        return "".join([ch for ch in s if ch.isalnum()]).lower() == "".join([ch for ch in s if ch.isalnum()])[::-1].lower()
+3        x = ''
+4        for i in range(len(s)-1, -1, -1):
+5            if s[i].isalnum():
+6                x += s[i].lower()
+7        return x == x[::-1]
