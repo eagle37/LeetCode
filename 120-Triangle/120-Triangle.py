1e8ceb46e2a44b4dc1eac7f2e09e2f4ce31f8270
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-05 17:19:53 +0530

    Create 120-Triangle

---
+++ 120-Triangle/120-Triangle.py
@@ -0,0 +1,12 @@
+# Last updated: 9/5/2026, 5:19:36 PM
+1class Solution:
+2    def maxProfit(self, prices: List[int]) -> int:
+3        minp = float("inf")
+4        maxp = 0
+5        for p in prices:
+6            if p < minp:
+7                minp = p
+8            p = p - minp
+9            if p > maxp:
+10                maxp = p
+11        return maxp
