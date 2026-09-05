2c69cf149c6a8a4fbcdedd54845bfff6dda8600c
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-05 17:29:28 +0530

    File updated at 9/5/2026, 5:29:11 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,10 +1,12 @@
-# Last updated: 9/5/2026, 5:27:13 PM
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        c = ""
-        for ch in s:
-            if ch.isalnum():
-                c += ch
-        return c == c[::-1]         
-        
+# Last updated: 9/5/2026, 5:29:11 PM
+1class Solution:
+2    def longestConsecutive(self, nums: List[int]) -> int:
+3        numset = set(nums)
+4        longest = 0
+5        for n in numset:
+6            if n - 1 not in numset:
+7                length = 1
+8                while n + length in numset:
+9                    length += 1
+10                longest = max(longest, length)
+11        return longest
