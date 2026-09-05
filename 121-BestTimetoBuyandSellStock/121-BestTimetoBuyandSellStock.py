68786abe4abcb450f6963c3835be1d6d66458f38
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-05 17:25:06 +0530

    File updated at 9/5/2026, 5:24:49 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,12 +1,4 @@
-# Last updated: 9/5/2026, 5:20:11 PM
+# Last updated: 9/5/2026, 5:24:49 PM
 1class Solution:
-2    def maxProfit(self, prices: List[int]) -> int:
-3        minp = float("inf")
-4        maxp = 0
-5        for p in prices:
-6            if p < minp:
-7                minp = p
-8            p = p - minp
-9            if p > maxp:
-10                maxp = p
-11        return maxp
+2    def isPalindrome(self, s: str) -> bool:
+3        return "".join([ch for ch in s if ch.isalnum()]).lower() == "".join([ch for ch in s if ch.isalnum()])[::-1].lower()
