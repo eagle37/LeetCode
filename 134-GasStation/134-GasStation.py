bc42c86b202d10e46d7d91942f465674f4e838ed
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-06 23:53:43 +0530

    Create 134-GasStation

---
+++ 134-GasStation/134-GasStation.py
@@ -0,0 +1,22 @@
+# Last updated: 9/6/2026, 11:53:25 PM
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+
+        total = 0
+        curr = 0
+        start = 0
+
+        for i in range(len(gas)):
+            diff = gas[i] - cost[i]
+
+            total += diff
+            curr += diff
+
+            if curr < 0:
+                start = i + 1
+                curr = 0
+
+        if total < 0:
+            return -1
+
+        return start
