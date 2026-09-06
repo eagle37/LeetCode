99517a789fa789911042a27632680c02349715df
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-06 22:50:05 +0530

    File updated at 9/6/2026, 10:49:47 PM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
@@ -1,22 +1,12 @@
-# Last updated: 9/6/2026, 10:48:17 PM
+# Last updated: 9/6/2026, 10:49:47 PM
 1class Solution:
-2    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-3
-4        total = 0
-5        curr = 0
-6        start = 0
-7
-8        for i in range(len(gas)):
-9            diff = gas[i] - cost[i]
-10
-11            total += diff
-12            curr += diff
-13
-14            if curr < 0:
-15                start = i + 1
-16                curr = 0
-17
-18        if total < 0:
-19            return -1
-20
-21        return start
+2    def longestConsecutive(self, nums: List[int]) -> int:
+3        numset = set(nums)
+4        longest = 0
+5        for n in numset:
+6            if n - 1 not in numset:
+7                length = 1
+8                while n + length in numset:
+9                    length += 1
+10                longest = max(longest, length)
+11        return longest
