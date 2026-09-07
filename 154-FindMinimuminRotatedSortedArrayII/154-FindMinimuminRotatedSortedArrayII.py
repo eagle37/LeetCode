5f2a21f76211e7ed92595143192a107ade977173
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-08 00:41:03 +0530

    File updated at 9/8/2026, 12:40:45 AM

--- 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.py
+++ 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.py
@@ -1,32 +1,20 @@
-# Last updated: 9/8/2026, 12:39:00 AM
-1class MinStack:
-2
-3    def __init__(self):
-4        self.stack = []
-5        self.minStack = []
+# Last updated: 9/8/2026, 12:40:45 AM
+1class Solution:
+2    def findMin(self, nums: List[int]) -> int:
+3        l = 0
+4        r = len(nums) - 1
+5        sm = nums[0]
 6
-7    def push(self, value: int) -> None:
-8        self.stack.append(value)
+7        while l <= r:
+8            mid = (l + r) // 2
 9
-10        if not self.minStack:
-11            self.minStack.append(value)
-12        else:
-13            self.minStack.append(min(value, self.minStack[-1]))
-14
-15    def pop(self) -> None:
-16        self.stack.pop()
-17        self.minStack.pop()
+10            sm = min(sm, nums[mid])
+11
+12            if nums[mid] < nums[r]:
+13                r = mid - 1
+14            elif nums[mid] == nums[r]:
+15                r -= 1
+16            else:
+17                l = mid + 1
 18
-19    def top(self) -> int:
-20        return self.stack[-1]
-21
-22    def getMin(self) -> int:
-23        return self.minStack[-1]
-24
-25
-26# Your MinStack object will be instantiated and called as such:
-27# obj = MinStack()
-28# obj.push(value)
-29# obj.pop()
-30# param_3 = obj.top()
-31# param_4 = obj.getMin()
+19        return sm
