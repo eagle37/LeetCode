887d3b0802623cc8a21f408824ff3dc70f23fe43
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-08 22:16:49 +0530

    File updated at 9/8/2026, 10:16:31 PM

--- 162-FindPeakElement/162-FindPeakElement.py
+++ 162-FindPeakElement/162-FindPeakElement.py
@@ -1,18 +1,14 @@
-# Last updated: 9/8/2026, 10:16:09 PM
+# Last updated: 9/8/2026, 10:16:31 PM
 1class Solution:
-2    def compareVersion(self, version1: str, version2: str) -> int:
-3        v1 = version1.split('.')
-4        v2 = version2.split('.')
-5
-6        n = max(len(v1), len(v2))
-7
-8        for i in range(n):
-9            a = int(v1[i]) if i < len(v1) else 0
-10            b = int(v2[i]) if i < len(v2) else 0
-11
-12            if a > b:
-13                return 1
-14            elif a < b:
-15                return -1
-16
-17        return 0
+2    def findPeakElement(self, nums: List[int]) -> int:
+3        if len(nums) == 1:
+4            return 0
+5        l = 0
+6        r = len(nums)-1
+7        while l < r:
+8            mid = (l+r)//2
+9            if nums[mid] < nums[mid+1]:
+10                l = mid+1
+11            elif nums[mid] > nums[mid+1]:
+12                r = mid
+13        return l
