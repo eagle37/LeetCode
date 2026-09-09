70b6783a32f27f2d07e3f4e8b036be6f1886fa53
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-09 22:37:56 +0530

    Create 204-CountPrimes

---
+++ 204-CountPrimes/204-CountPrimes.py
@@ -0,0 +1,18 @@
+# Last updated: 9/9/2026, 10:37:38 PM
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+
+        isPrime = [True] * n
+        isPrime[0] = isPrime[1] = False
+
+        i = 2
+
+        while i * i < n:
+            if isPrime[i]:
+                for j in range(i * i, n, i):
+                    isPrime[j] = False
+            i += 1
+
+        return sum(isPrime)
