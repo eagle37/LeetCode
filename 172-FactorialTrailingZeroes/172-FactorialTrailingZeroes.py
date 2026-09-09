15f82ec71b844e17082b867557f4c7a574a4b4d8
eagle37 <84372934+eagle37@users.noreply.github.com>
2026-09-09 22:21:30 +0530

    File updated at 9/9/2026, 10:21:12 PM

--- 172-FactorialTrailingZeroes/172-FactorialTrailingZeroes.py
+++ 172-FactorialTrailingZeroes/172-FactorialTrailingZeroes.py
@@ -1,11 +1,11 @@
-# Last updated: 9/9/2026, 10:19:22 PM
+# Last updated: 9/9/2026, 10:21:12 PM
 1class Solution:
 2    def trailingZeroes(self, n: int) -> int:
-3        ans = 1
-4        for i in range(1, n+1):
-5            ans *= i
-6        cnt = 0
-7        while ans % 10 == 0:
-8            cnt += 1
-9            ans = ans // 10
-10        return cnt
+3        count = 0
+4        power = 5
+5
+6        while power <= n:
+7            count += n//power
+8            power *= 5
+9        
+10        return count
